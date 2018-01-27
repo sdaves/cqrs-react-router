6bde3597fc89b5b163c2afa4fbe465baa36eb78b
Stephen Daves <contact@stephendaves.com>
2018-01-27 12:25:58 -0500

    Add runtime symbol for dependency injection

--- src/interfaces/iamavalueobject.ts
+++ src/interfaces/iamavalueobject.ts
@@ -3,3 +3,6 @@ export interface IAmAValueObject<T>{
     equals(other: T): boolean;
 
 }
+
+// Symbol used during runtime for dependency injection
+export const IAmAValueObject = Symbol('IAmAValueObject')
