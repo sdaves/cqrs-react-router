de70826c709ea50b0e49b154aaf645271fb86feb
Stephen Daves <contact@stephendaves.com>
2018-01-27 12:24:25 -0500

    Add runtime symbol for dependency injection

--- src/interfaces/iamacommandhandler.ts
+++ src/interfaces/iamacommandhandler.ts
@@ -6,3 +6,6 @@ export interface IAmACommandHandler{
 
     handle(command: IAmACommand, domainService: IDomainService, callback: (command: IAmACommand) => void): void;
 }
+
+// Symbol used during runtime for dependency injection
+export const IAmACommandHandler = Symbol('IAmACommandHandler')
