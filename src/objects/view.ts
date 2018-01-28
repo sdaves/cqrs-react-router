a6159d5039d28de79efbe1bd145834b72a356184
Stephen Daves <contact@stephendaves.com>
2018-01-27 19:55:46 -0500

    Add iview usage to view class

--- src/objects/view.ts
+++ src/objects/view.ts
@@ -1,6 +1,7 @@
 import {IAmADomainEvent} from "../interfaces/iamadomainevent";
+import {IView} from "../interfaces/iview";
 
-export abstract class View{
+export abstract class View implements IView {
 
     abstract name: string;
 
