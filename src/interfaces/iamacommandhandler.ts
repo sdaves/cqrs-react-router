b5ace2d17679332cd07174673640731da5ffad61
Stephen Daves <contact@stephendaves.com>
2018-01-27 19:52:59 -0500

    Add iview usage to command handler interface

--- src/interfaces/iamacommandhandler.ts
+++ src/interfaces/iamacommandhandler.ts
@@ -1,9 +1,15 @@
 import {IAmACommand} from "./iamacommand";
 import {IDomainService} from "./idomainservice";
+import {IView} from "./iview";
 
 export interface IAmACommandHandler{
     commandNames: string[];
-    handle(command: IAmACommand, domainService: IDomainService, callback: (command: IAmACommand) => void): void;
+    handle(
+        command: IAmACommand, 
+        domainService: IDomainService, 
+        callback: (command: IAmACommand) => void, 
+        getViewByName?: (name: string, viewCallBack: (view: IView) => void) => void
+    ): void;
 }
 
 // Symbol used during runtime for dependency injection
